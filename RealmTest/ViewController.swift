e31e2fdc5b921fca34feb017dc1903476302b7e4
toaster08 <y10star.hon@gmail.com>
2021-02-20 15:01:11 +0900

    Initial Commit

---
+++ RealmTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealmTest
+//
+//  Created by 山田　天星 on 2021/02/20.
+//  Copyright © 2021 toaster. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
